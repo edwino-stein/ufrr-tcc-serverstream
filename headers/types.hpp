da9460158160182078f91c434fab8ebc15b4bfee
Edwino Stein <edwino.stein@gmail.com>
2019-03-19 21:23:08 -0400

    Adicionado arquivo headers/types.hpp

---
+++ headers/types.hpp
@@ -0,0 +1,10 @@
+#if !defined  _TYPES_HPP
+    #define _TYPES_HPP
+    
+    #include <string>
+    #include <vector>
+
+    using String = std::string;
+    template <class T> using Vector = std::vector<T>;
+
+#endif
