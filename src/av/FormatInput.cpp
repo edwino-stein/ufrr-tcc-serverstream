d1f819283b25c5aa9f78ecf35a2667c807564caf
Edwino Stein <edwino.stein@gmail.com>
2019-03-21 18:52:00 -0400

    Adicionando suporte a exceptions em av::FormatInput e av::FormatOutput

--- src/av/FormatInput.cpp
+++ src/av/FormatInput.cpp
@@ -1,21 +1,25 @@
 #include "av/FormatInput.hpp"
 
+#include "exceptions/Exception.hpp"
+#include "exceptions/AvErrorException.hpp"
+#include "exceptions/ReturnValueException.hpp"
+
 using namespace av;
+
+using exceptions::Exception;
+using exceptions::AvErrorException;
+using exceptions::ReturnValueException;
 
 FormatInput::FormatInput() : Format(), format(_format){
     this->_format = NULL;
 }
 
-FormatInput::~FormatInput(){
-    // TODO: verificar como liberar a memoria do jeito certo
-}
+FormatInput::~FormatInput(){}
 
 void FormatInput::read(AVPacket *packet) const {
 
     int r = av_read_frame(this->formatCtx, packet);
 
-    if (r < 0){
-        // TODO: exceptions
-        throw r;
-    }
+    if(r < 0)
+        throw AvErrorException(r, ReturnValueException("av_read_frame", r));
 }
--- src/av/FormatOutput.cpp
+++ src/av/FormatOutput.cpp
@@ -1,21 +1,26 @@
 #include "av/FormatOutput.hpp"
 
+#include "exceptions/Exception.hpp"
+#include "exceptions/AvErrorException.hpp"
+#include "exceptions/ReturnValueException.hpp"
+
 using namespace av;
+
+using exceptions::Exception;
+using exceptions::AvErrorException;
+using exceptions::ReturnValueException;
 
 FormatOutput::FormatOutput() : Format(), format(_format){
     this->_format = NULL;
 }
 
-FormatOutput::~FormatOutput(){
-    // TODO: verificar como liberar a memoria do jeito certo
-}
+FormatOutput::~FormatOutput(){}
 
 void FormatOutput::write(AVPacket *packet) const {
 
     int r = av_interleaved_write_frame(this->formatCtx, packet);
 
-    if (r < 0){
-        // TODO: exceptions
-        throw r;
-    }
+    if(r < 0)
+        if (r < 0)
+        throw AvErrorException(r, ReturnValueException("av_interleaved_write_frame", r));
 }
