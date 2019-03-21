0e9034af7c0dba516b53e31cc802836d55627ced
Edwino Stein <edwino.stein@gmail.com>
2019-03-21 18:33:31 -0400

    Adiciondo suporte a std::function

--- headers/types.hpp
+++ headers/types.hpp
@@ -3,10 +3,12 @@
     
     #include <string>
     #include <vector>
+    #include <functional>
     #include "ArduinoJson/ArduinoJson.h"
 
     using String = std::string;
     template <class T> using Vector = std::vector<T>;
+    template <class T> using AFunction = std::function<T>;
     namespace json = ArduinoJson;
 
 #endif
