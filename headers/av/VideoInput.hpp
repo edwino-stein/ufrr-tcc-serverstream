46efbf8fb7a598235426ba83aaf077a5b0d9a55c
Edwino Stein <edwino.stein@gmail.com>
2019-03-21 18:52:48 -0400

    Refatorado e adicionado métodos para ciclio de vida em av::VideoInput

--- headers/av/VideoInput.hpp
+++ headers/av/VideoInput.hpp
@@ -8,14 +8,23 @@
 
         class VideoInput : public av::CodecVideo, public av::FormatInput {
 
+            protected:
+                virtual void openFile(String &fileName) override;
+                virtual void initFormat(String demuxer, String codec) override;
+                virtual void initVideoStream() override;
+                virtual void initCodec() override;
+                virtual void initCodec(AFunction<void(AVCodecContext *, AVCodec *)> setupCodec) override;
+
+                virtual void closeFile() override;
+                virtual void closeFormat() override;
+                virtual void closeCodec() override;
+
             public:
 
                 VideoInput();
                 virtual ~VideoInput();
 
                 virtual void rescaleTs(AVPacket *packet) const override;
-                virtual void close() override;
-
                 virtual void decode(AVPacket *packet, DecodeListener *listener) const;
         };
     }
--- src/av/VideoInput.cpp
+++ src/av/VideoInput.cpp
@@ -1,7 +1,15 @@
 #include "av/VideoInput.hpp"
 #include "av/DecodeListener.hpp"
 
+#include "exceptions/Exception.hpp"
+#include "exceptions/AvErrorException.hpp"
+#include "exceptions/ReturnValueException.hpp"
+
 using namespace av;
+
+using exceptions::Exception;
+using exceptions::AvErrorException;
+using exceptions::ReturnValueException;
 
 VideoInput::VideoInput() : CodecVideo(), FormatInput(){}
 VideoInput::~VideoInput(){}
@@ -12,11 +20,6 @@ void VideoInput::rescaleTs(AVPacket *packet) const {
         this->videoStream->time_base,
         this->codecCtx->time_base
     );
-}
-
-void VideoInput::close(){
-    Codec::close();
-    Format::close();
 }
 
 void VideoInput::decode(AVPacket *packet, DecodeListener *listener) const {
@@ -34,4 +37,110 @@ void VideoInput::decode(AVPacket *packet, DecodeListener *listener) const {
     }
 
     av_frame_free(&frame);
+}
+
+void VideoInput::openFile(String &filename){
+
+    int r = 0;
+
+    r = avformat_open_input(
+        &(this->_formatCtx),
+        filename.c_str(),
+        NULL,
+        NULL
+    );
+
+    if(r < 0)
+        throw AvErrorException(r, ReturnValueException("avformat_open_input", r));
+    
+    this->initFormat("", "");
+}
+
+void VideoInput::initFormat(String demuxer, String codec){
+
+    if(this->formatCtx == NULL)
+        throw AvErrorException(AVERROR_DEMUXER_NOT_FOUND, Exception("Video format not initialized."));
+
+    int r = 0;
+    r = avformat_find_stream_info(this->formatCtx, NULL);
+    if(r < 0)
+        throw AvErrorException(r, ReturnValueException("avformat_open_input", r));
+
+    this->_format = this->formatCtx->iformat;
+}
+
+void VideoInput::initVideoStream(){
+
+    if(this->formatCtx == NULL)
+        throw AvErrorException(AVERROR_STREAM_NOT_FOUND, Exception("Video format not initialized."));
+   
+    int r = 0;
+
+    r = av_find_best_stream(
+        this->formatCtx,
+        AVMEDIA_TYPE_VIDEO,
+        -1,
+        -1,
+        NULL,
+        0
+    );
+
+    if(r < 0) throw AvErrorException(r, ReturnValueException("av_find_best_stream", r));
+
+    this->_videoStream = this->formatCtx->streams[r];
+    this->_videoStreamIdx = r;
+}
+
+void VideoInput::initCodec(){
+
+    if(this->videoStream == NULL)
+        throw AvErrorException(AVERROR_DECODER_NOT_FOUND, AvErrorException(AVERROR_STREAM_NOT_FOUND));
+
+    if(this->formatCtx == NULL)
+        throw AvErrorException(AVERROR_DECODER_NOT_FOUND, Exception("Video format not initialized"));
+    
+    int r = 0;
+
+    this->_codec = avcodec_find_decoder(this->videoStream->codecpar->codec_id);
+    if(this->codec == NULL)
+        throw AvErrorException(AVERROR_DECODER_NOT_FOUND, ReturnValueException("avcodec_find_decoder", "NULL"));
+
+    this->_codecCtx = avcodec_alloc_context3(this->codec);
+    if(this->_codecCtx == NULL)
+        throw AvErrorException(AVERROR_DECODER_NOT_FOUND, ReturnValueException("avcodec_alloc_context3", "NULL"));
+
+    r = avcodec_parameters_to_context(this->codecCtx, this->videoStream->codecpar);
+    if(r < 0)
+        throw AvErrorException(r, ReturnValueException("avcodec_parameters_to_context", r));
+
+    this->codecCtx->framerate = av_guess_frame_rate(this->formatCtx, this->videoStream, NULL);
+    if(this->codecCtx->framerate.num == 0 && this->codecCtx->framerate.den == 1)
+        throw AvErrorException(AVERROR_DECODER_NOT_FOUND, Exception("Invalid framerate"));
+
+    r = avcodec_open2(this->codecCtx, this->codec, NULL);
+    if (r < 0)
+        throw AvErrorException(r, ReturnValueException("avcodec_parameters_to_context", r));
+}
+
+void VideoInput::initCodec(AFunction<void(AVCodecContext *, AVCodec *)> setupCodec){
+    this->initCodec();
+}
+
+void VideoInput::closeFile(){
+    this->closeFormat();
+}
+
+void VideoInput::closeFormat(){
+    if(this->formatCtx != NULL){
+        avformat_close_input(&(this->_formatCtx));
+        this->_format = NULL;
+    }
+}
+
+void VideoInput::closeCodec(){
+    if(this->codecCtx != NULL){
+        avcodec_close(this->codecCtx);
+        avcodec_free_context(&(this->_codecCtx));
+        this->_codec = NULL;
+    }
 }
