4e2b54d715df2c5c9593ad418e01709b04eb2075
Edwino Stein <edwino.stein@gmail.com>
2019-03-29 19:19:31 -0400

    Adicionado o tipo Mutex ao arquivo types.hpp

--- headers/types.hpp
+++ headers/types.hpp
@@ -1,14 +1,16 @@
 #if !defined  _TYPES_HPP
     #define _TYPES_HPP
-    
+
     #include <string>
     #include <vector>
     #include <functional>
+    #include <mutex>
     #include "ArduinoJson/ArduinoJson.h"
 
     using String = std::string;
     template <class T> using Vector = std::vector<T>;
     template <class T> using AFunction = std::function<T>;
+    using Mutex = std::mutex;
     namespace json = ArduinoJson;
 
 #endif
