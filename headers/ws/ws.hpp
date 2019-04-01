2a5920b4f8169abf374dc62a24626f0208dc17b4
Edwino Stein <edwino.stein@gmail.com>
2019-04-01 15:33:45 -0400

    Adicionado alguns alias de tipos do boost.beast [issue #2]

--- headers/ws/ws.hpp
+++ headers/ws/ws.hpp
@@ -7,6 +7,17 @@
     namespace ws {
 
         //Boost.beast alias types
+        namespace http = boost::beast::http;
+        using tcp = boost::asio::ip::tcp;
+        using TCPSocket = tcp::socket;
+        using WSocket = boost::beast::websocket::stream<tcp::socket>;
+        using Buffer = boost::beast::multi_buffer;
+        using IOContext = boost::asio::io_context;
+        using EndPoint = tcp::endpoint;
+        using Acceptor = tcp::acceptor;
+        using HTTPRequest = http::request<http::string_body>;
+        using HTTPWSResponse = boost::beast::websocket::response_type;
+        using FlatBuffer = boost::beast::flat_buffer;
 
         //Functions
 
