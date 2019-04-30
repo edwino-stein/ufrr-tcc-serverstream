2d0790978a7bb208283add69c4f385dd5fa03758
Edwino Stein <edwino.stein@gmail.com>
2019-04-30 18:05:45 -0400

    Refatorada a classe UnixMessageQueueListener [Closes #20]

--- headers/runtime/UnixMessageQueueListener.hpp
+++ headers/runtime/UnixMessageQueueListener.hpp
@@ -5,40 +5,97 @@
     #include "runtime/LoopInside.hpp"
 
     #include <sys/types.h>
-     #include <sys/ipc.h>
 
     namespace runtime {
 
+        namespace UnixMessageQueue {
+            key_t fileToKey(String fileName, int projId);
+            int getQueue(key_t key, int flags);
+            int createQueue(key_t key, int flags);
+            void removeQueue(int msqId);
+            bool existsQueue(key_t key);
+            bool popQueue(int msqId, void *msgP, size_t msgSz, const bool noWait);
+        }
+
+        template <size_t S>
         class UnixMessageQueueListener : public runtime::LoopInside {
 
             protected:
 
                 struct MsgBuffer {
                     long mtype;
-                    char mtext[1];
+                    char mtext[S];
                 };
 
                 key_t key;
                 AFunction<void(String &msg)> callback;
-                const size_t buffSize;
                 int msqId;
 
                 void init(key_t key, const bool recreate);
                 void loop() override;
 
-                static int getQueue(key_t key, int flags);
-                static int createQueue(key_t key, int flags);
-                static bool queueExists(key_t key);
-                static void removeQueue(int msqId);
-
             public:
 
-                UnixMessageQueueListener(key_t key, AFunction<void(String &msg)> callback, const size_t buffSize = 255, const bool recreate = true);
-                UnixMessageQueueListener(String fileName, AFunction<void(String &msg)> callback, const size_t buffSize = 255, const bool recreate = true);
+                UnixMessageQueueListener();
+                UnixMessageQueueListener(key_t key, AFunction<void(String &msg)> callback, const bool recreate = true);
+                UnixMessageQueueListener(String fileName, AFunction<void(String &msg)> callback, const bool recreate = true);
                 virtual ~UnixMessageQueueListener();
 
                 void destroyQueue() const;
         };
+
+
+        template <size_t S>
+        UnixMessageQueueListener<S>::UnixMessageQueueListener(key_t key, AFunction<void(String &msg)> callback, const bool recreate):
+        LoopInside(), callback(callback){
+            this->init(key, recreate);
+        }
+
+        template <size_t S>
+        UnixMessageQueueListener<S>::UnixMessageQueueListener(String fileName, AFunction<void(String &msg)> callback, const bool recreate):
+        LoopInside(), callback(callback){
+            this->init(UnixMessageQueue::fileToKey(fileName, 'G'), recreate);
+        }
+
+        template <size_t S>
+        UnixMessageQueueListener<S>::~UnixMessageQueueListener(){
+            this->stop(true);
+        }
+
+        template <size_t S>
+        void UnixMessageQueueListener<S>::init(key_t key, const bool recreate){
+
+            this->key = key;
+
+            if(UnixMessageQueue::existsQueue(this->key)){
+
+                this->msqId = UnixMessageQueue::getQueue(this->key, 0);
+
+                if(recreate){
+                    UnixMessageQueue::removeQueue(this->msqId);
+                    this->msqId = UnixMessageQueue::createQueue(this->key, 0666);
+                }
+            }
+            else{
+                this->msqId = UnixMessageQueue::createQueue(this->key, 0666);
+            }
+        }
+
+        template <size_t S>
+        void UnixMessageQueueListener<S>::destroyQueue() const {
+            UnixMessageQueue::removeQueue(this->msqId);
+        }
+
+        template <size_t S>
+        void UnixMessageQueueListener<S>::loop(){
+
+            struct MsgBuffer buf;
+            if(!UnixMessageQueue::popQueue(this->msqId, &buf, S + 1, true)) return;
+
+            buf.mtext[S] = '\0';
+            String str(buf.mtext);
+            this->callback(str);
+        }
     }
 
 #endif
--- src/runtime/UnixMessageQueueListener.cpp
+++ src/runtime/UnixMessageQueueListener.cpp
@@ -1,88 +1,45 @@
 #include "runtime/UnixMessageQueueListener.hpp"
 
 #include <errno.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-#include <iostream>
-#include <sstream>
 
 using namespace runtime;
 
-UnixMessageQueueListener::UnixMessageQueueListener(key_t key, AFunction<void(String &msg)> callback, const size_t buffSize, const bool recreate):
-LoopInside(), callback(callback), buffSize(buffSize){
-    this->init(key, recreate);
-}
-UnixMessageQueueListener::UnixMessageQueueListener(String fileName, AFunction<void(String &msg)> callback, const size_t buffSize, const bool recreate):
-LoopInside(), callback(callback), buffSize(buffSize){
-    this->init(ftok(fileName.c_str(), 'G'), recreate);
+bool UnixMessageQueue::popQueue(int msqId, void *msgP, size_t msgSz, const bool noWait){
+    int msgFlg = 0;
+    if(noWait) msgFlg |= IPC_NOWAIT;
+    return msgrcv(msqId, msgP, msgSz, 0, msgFlg) != -1;
 }
 
-UnixMessageQueueListener::~UnixMessageQueueListener(){
-    this->stop(true);
+key_t UnixMessageQueue::fileToKey(String fileName, int projId){
+    return ftok(fileName.c_str(), projId);
 }
 
-void UnixMessageQueueListener::init(key_t key, const bool recreate){
-
-    this->key = key;
-
-    if(UnixMessageQueueListener::queueExists(this->key)){
-
-        this->msqId = UnixMessageQueueListener::getQueue(this->key, 0);
-
-        if(recreate){
-            UnixMessageQueueListener::removeQueue(this->msqId);
-            this->msqId = UnixMessageQueueListener::createQueue(this->key, 0666);
-        }
-    }
-    else{
-        this->msqId = UnixMessageQueueListener::createQueue(this->key, 0666);
-    }
-}
-
-void UnixMessageQueueListener::loop(){
-
-    struct MsgBuffer buf;
-    std::ostringstream oss;
-
-    if(msgrcv(this->msqId, &buf, this->buffSize + 1, 0, 0 | IPC_NOWAIT) == -1)
-        return;
-
-    buf.mtext[this->buffSize] = '\0';
-    oss << buf.mtext;
-
-    String str(oss.str());
-    this->callback(str);
-}
-
-void UnixMessageQueueListener::destroyQueue() const {
-    UnixMessageQueueListener::removeQueue(this->msqId);
-}
-
-int UnixMessageQueueListener::getQueue(key_t key, int flags){
+int UnixMessageQueue::getQueue(key_t key, int flags){
     return msgget(key, flags);
 }
 
-int UnixMessageQueueListener::createQueue(key_t key, int flags){
-    return UnixMessageQueueListener::getQueue(key, flags | IPC_CREAT | IPC_EXCL);
+int UnixMessageQueue::createQueue(key_t key, int flags){
+    return UnixMessageQueue::getQueue(key, flags | IPC_CREAT | IPC_EXCL);
 }
 
-bool UnixMessageQueueListener::queueExists(key_t key){
+void UnixMessageQueue::removeQueue(int msqId){
+    msgctl(msqId, IPC_RMID, NULL);
+}
 
-    int msqId = UnixMessageQueueListener::createQueue(key, 0);
+bool UnixMessageQueue::existsQueue(key_t key){
+
+    int msqId = UnixMessageQueue::createQueue(key, 0);
 
     if(msqId == -1){
         if(errno == EEXIST) return true;
         if(errno == EACCES) return true;
     }
     else{
-        UnixMessageQueueListener::removeQueue(msqId);
+        UnixMessageQueue::removeQueue(msqId);
     }
 
     return false;
-}
-
-void UnixMessageQueueListener::removeQueue(int msqId){
-    msgctl(msqId, IPC_RMID, NULL);
 }
