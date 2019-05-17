9aafab5a52690cebbbae89a1b793b983552c311d
Edwino Stein <edwino.stein@gmail.com>
2019-05-16 22:43:20 -0400

    Corrigindo erro de compilação no Raspberry PI [Closes #24]

--- headers/av/libav.hpp
+++ headers/av/libav.hpp
@@ -15,6 +15,11 @@
         #include <libavfilter/buffersrc.h>
     }
 
+    //For missing avio_context_free on old versions
+    #if LIBAVFORMAT_VERSION_INT <= AV_VERSION_INT(57, 56, 101)
+        void avio_context_free(AVIOContext **ps);
+    #endif
+
     namespace av {
 
         //Functions
--- src/av/libav.cpp
+++ src/av/libav.cpp
@@ -1,8 +1,15 @@
 #include "av/libav.hpp"
 
 void av::initAll(){
-    av_register_all(); 
+    av_register_all();
     avcodec_register_all();
     avdevice_register_all();
     avfilter_register_all();
 }
+
+//For missing avio_context_free on old versions
+#if LIBAVFORMAT_VERSION_INT <= AV_VERSION_INT(57, 56, 101)
+void avio_context_free(AVIOContext **ps){
+    av_freep(ps);
+}
+#endif
